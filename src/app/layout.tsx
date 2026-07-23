ea626cc9884d4e21b1ffd53f9649f1bd67d28b74
Daniel Ulloa <danielulloam@gmail.com>
2026-07-22 22:01:41 -0500

    chore: cambio de foto y titulo de pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Tangerine, Inter } from "next/font/google";
 
-export const metadata: Metadata = { title: "Nuestro Sí para toda la vida" };
+export const metadata: Metadata = { title: "Mis 15 primaveras" };
 
 // Paleta base para layout
 const SOFT_BG_APP = "#F7FBFE";
